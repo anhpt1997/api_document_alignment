d7373848ad22f43837e80e9fda410d1bca7e8341
anhpt1997 <user email>
2020-12-03 22:38:41 +0700

    FdF

--- api.py
+++ api.py
@@ -17,13 +17,13 @@ def document_align_api():
     writeListDocToFile(listDoc_vn_raw , 'file_vn_raw_indexed.txt')
     writeListDocToFile(listDoc_khrme_raw , 'file_khrme_raw_indexed.txt')
 
-    splitAndWriteDocToFileBySegmentLength(file_in = 'file_khrme_raw_indexed.txt' , file_out = 'file_khrme_raw_indexed_segmented.txt' , segmentLength= 1000)
+    splitAndWriteDocToFileBySegmentLength(file_in = 'file_khrme_raw_indexed.txt' , file_out = 'data/file_khrme_raw_indexed_segmented.txt' , segmentLength= 1000)
 
-    translate_file(file_in = 'file_khrme_raw_indexed_segmented.txt' , file_out = 'file_khrme_raw_indexed_segmented_translated.txt')
+    translate_file(file_in = 'data/file_khrme_raw_indexed_segmented.txt' , file_out = 'data/file_khrme_raw_indexed_segmented_translated.txt')
 
-    readAndConcateSegmentToDocFromFile(file_in = 'file_khrme_raw_indexed_segmented_translated.txt' , file_out ='file_vn_translated_indexed.txt')
+    readAndConcateSegmentToDocFromFile(file_in = 'data/file_khrme_raw_indexed_segmented_translated.txt' , file_out ='data/file_vn_translated_indexed.txt')
     # listDoc_vn_raw = readListDocFromFile(file = "test_raw_vn.txt")
-    listDoc_vn_translate = readLinesFromFile('file_vn_translated_indexed.txt')
+    listDoc_vn_translate = readLinesFromFile('data/file_vn_translated_indexed.txt')
 
     vocabW2vec = getWord2Vec()
 
@@ -33,7 +33,7 @@ def document_align_api():
 
     print(resultMatrixSim)
 
-    getPairDocFromResult(resultMatrixSim , listDoc_vn_raw , listDoc_khrme_raw, 'resultDocAlign.txt', thresol= 0.9)
+    getPairDocFromResult(resultMatrixSim , listDoc_vn_raw , listDoc_khrme_raw, 'result/resultDocAlign.txt', thresol= 0.9)
 
 
 if __name__ == "__main__":
